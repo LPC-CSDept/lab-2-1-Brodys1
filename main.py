d6e68441fedb2615782d5e83af01ead6e754e9a6
Brody <bdsmith0010@zonemail.clpccd.edu>
2023-02-06 16:29:27 -0800

    lab 1

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 def main():
    ##################################################
    # Comlete your code here
+    original_price = int(input("What's the regular price? "))
+    rate = int(input("What's the discount percentage? "))/100
+
+    discount_amount = int(original_price * rate)
+    final_price = int(original_price - (original_price * rate))
+    print ('Regular Price : $', original_price,
+    '\nDiscount Amount : $', discount_amount,
+    '\nThe Final Price : $', final_price)
    ##################################################
 
     pass
